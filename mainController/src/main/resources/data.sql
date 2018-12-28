3b0b634fe503babeed612dc14d9136b1be3acd4b
sjlee7373 <sjlee7373@gmail.com>
2018-12-28 18:26:10 +0900

    test

--- mainController/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO food (id, name, price, spicylevel) VALUES (1, 'Kimchi Soup', 10000, 3);
-INSERT INTO food (id, name, price, spicylevel) VALUES (2, 'Seafood Pancake', 10000, 1);
-INSERT INTO food (id, name, price, spicylevel) VALUES (3, 'Pork BBQ', 10000, 0);
-INSERT INTO food (id, name, price, spicylevel) VALUES (4, 'Chicken Soup with Abalone', 10000, 0);
-INSERT INTO food (id, name, price, spicylevel) VALUES (5, 'Bulgogi', 10000, 3);
-INSERT INTO food (id, name, price, spicylevel) VALUES (6, 'Beef BBQ', 10000, 0);
