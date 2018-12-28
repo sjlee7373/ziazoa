0b6312a87cdf754b790fc859869b5f5d9fa23ea3
sjlee7373 <sjlee7373@gmail.com>
2018-12-28 16:52:10 +0900

    test

---
+++ configserver/src/test/java/zia/framework/maincontroller/MainControllerApplicationTests.java
@@ -0,0 +1,17 @@
+package zia.framework.maincontroller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MainControllerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
